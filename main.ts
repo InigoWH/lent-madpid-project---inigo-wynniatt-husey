32ff3153c9aa5dacd283da1bc03ccf59b21085c0
InigoWH <77437933+InigoWH@users.noreply.github.com>
2021-01-16 17:03:22 +0000

    Added virus/sprite/text

--- main.ts
+++ main.ts
@@ -1,1 +1,20 @@
-
+let mySprite = sprites.create(img`
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . e e e e e e . . . . . . 
+    . . . e e e e e e e e . . . . . 
+    . . . e d d d d d d e . . . . . 
+    . . . . d 8 d d 8 d . . . . . . 
+    . . . . f f f f f f . . . . . . 
+    . . . . . f f f f . . . . . . . 
+    . . . 7 7 d f f d 7 7 . . . . . 
+    . . . 7 7 7 d d 7 7 7 . . . . . 
+    . . . 7 . 7 7 7 7 . 7 . . . . . 
+    . . . 7 . 7 7 7 7 . 7 . . . . . 
+    . . . d . 7 7 7 7 . d . . . . . 
+    . . . . . 8 8 8 8 . . . . . . . 
+    . . . . . 8 . . 8 . . . . . . . 
+    . . . . f f . . f f . . . . . . 
+    `, SpriteKind.Player)
+game.showLongText("Press 'A' to continue", DialogLayout.Top)
+game.showLongText("You have been summoned to save the human race from a deadly disease called COVID-19. Against all peril you must find the components to create a vaccine avoiding contagious people and viruses along the way", DialogLayout.Top)
