4cffb6c18311cfce6de537772ee3a724bf0c4610
InigoWH <77437933+InigoWH@users.noreply.github.com>
2021-01-28 10:31:38 +0000

    Functions, started movement for enemy

--- main.ts
+++ main.ts
@@ -1,6 +1,17 @@
+enum ActionKind {
+    Walking,
+    Idle,
+    Jumping
+}
 namespace SpriteKind {
     export const h = SpriteKind.create()
 }
+scene.onOverlapTile(SpriteKind.Player, assets.tile`tile`, function (sprite, location) {
+    if (Checkpoint_counter < 14) {
+        Checkpoint_counter += 1
+        game.showLongText("Checkpoint 13 reached", DialogLayout.Top)
+    }
+})
 controller.up.onEvent(ControllerButtonEvent.Pressed, function () {
     if (person.vy == 0) {
         person.vy = -150
@@ -14,139 +25,11 @@ scene.onOverlapTile(SpriteKind.Player, assets.tile`tile14`, function (sprite, lo
 })
 scene.onOverlapTile(SpriteKind.Player, assets.tile`tile7`, function (sprite, location) {
     game.showLongText("You have caught the virus from the crowd! You have failed your citizens! You are transported back to the nearest hospital.", DialogLayout.Center)
-    if (Checkpoint_counter == 1) {
-        tiles.placeOnRandomTile(person, assets.tile`tile8`)
-        info.changeLifeBy(-1)
-        music.powerDown.play()
-    }
-    if (Checkpoint_counter == 2) {
-        tiles.placeOnRandomTile(person, assets.tile`tile9`)
-        info.changeLifeBy(-1)
-        music.powerDown.play()
-    }
-    if (Checkpoint_counter == 3) {
-        tiles.placeOnRandomTile(person, assets.tile`tile10`)
-        info.changeLifeBy(-1)
-        music.powerDown.play()
-    }
-    if (Checkpoint_counter == 4) {
-        tiles.placeOnRandomTile(person, assets.tile`tile11`)
-        info.changeLifeBy(-1)
-        music.powerDown.play()
-    }
-    if (Checkpoint_counter == 5) {
-        tiles.placeOnRandomTile(person, assets.tile`tile14`)
-        info.changeLifeBy(-1)
-        music.powerDown.play()
-    }
-    if (Checkpoint_counter == 6) {
-        tiles.placeOnRandomTile(person, assets.tile`tile15`)
-        info.changeLifeBy(-1)
-        music.powerDown.play()
-    }
-    if (Checkpoint_counter == 7) {
-        tiles.placeOnRandomTile(person, assets.tile`tile16`)
-        info.changeLifeBy(-1)
-        music.powerDown.play()
-    }
-    if (Checkpoint_counter == 8) {
-        tiles.placeOnRandomTile(person, assets.tile`tile17`)
-        info.changeLifeBy(-1)
-        music.powerDown.play()
-    }
-    if (Checkpoint_counter == 9) {
-        tiles.placeOnRandomTile(person, assets.tile`tile18`)
-        info.changeLifeBy(-1)
-        music.powerDown.play()
-    }
-    if (Checkpoint_counter == 10) {
-        tiles.placeOnRandomTile(person, assets.tile`tile19`)
-        info.changeLifeBy(-1)
-        music.powerDown.play()
-    }
-    if (Checkpoint_counter == 11) {
-        tiles.placeOnRandomTile(person, assets.tile`tile23`)
-        info.changeLifeBy(-1)
-        music.powerDown.play()
-    }
-    if (Checkpoint_counter == 12) {
-        tiles.placeOnRandomTile(person, assets.tile`tile24`)
-        info.changeLifeBy(-1)
-        music.powerDown.play()
-    }
-    if (Checkpoint_counter == 13) {
-        tiles.placeOnRandomTile(person, assets.tile`tile25`)
-        info.changeLifeBy(-1)
-        music.powerDown.play()
-    }
+    doSomething()
 })
 scene.onOverlapTile(SpriteKind.Player, assets.tile`tile3`, function (sprite, location) {
     game.showLongText("You have caught the virus lurking in the air. You have failed! You are transported back to the nearest hospital.", DialogLayout.Center)
-    if (Checkpoint_counter == 1) {
-        tiles.placeOnRandomTile(person, assets.tile`tile8`)
-        info.changeLifeBy(-1)
-        music.powerDown.play()
-    }
-    if (Checkpoint_counter == 2) {
-        tiles.placeOnRandomTile(person, assets.tile`tile9`)
-        info.changeLifeBy(-1)
-        music.powerDown.play()
-    }
-    if (Checkpoint_counter == 3) {
-        tiles.placeOnRandomTile(person, assets.tile`tile10`)
-        info.changeLifeBy(-1)
-        music.powerDown.play()
-    }
-    if (Checkpoint_counter == 4) {
-        tiles.placeOnRandomTile(person, assets.tile`tile11`)
-        info.changeLifeBy(-1)
-        music.powerDown.play()
-    }
-    if (Checkpoint_counter == 5) {
-        tiles.placeOnRandomTile(person, assets.tile`tile14`)
-        info.changeLifeBy(-1)
-        music.powerDown.play()
-    }
-    if (Checkpoint_counter == 6) {
-        tiles.placeOnRandomTile(person, assets.tile`tile15`)
-        info.changeLifeBy(-1)
-        music.powerDown.play()
-    }
-    if (Checkpoint_counter == 7) {
-        tiles.placeOnRandomTile(person, assets.tile`tile16`)
-        info.changeLifeBy(-1)
-        music.powerDown.play()
-    }
-    if (Checkpoint_counter == 8) {
-        tiles.placeOnRandomTile(person, assets.tile`tile17`)
-        info.changeLifeBy(-1)
-        music.powerDown.play()
-    }
-    if (Checkpoint_counter == 9) {
-        tiles.placeOnRandomTile(person, assets.tile`tile18`)
-        info.changeLifeBy(-1)
-        music.powerDown.play()
-    }
-    if (Checkpoint_counter == 10) {
-        tiles.placeOnRandomTile(person, assets.tile`tile19`)
-        info.changeLifeBy(-1)
-        music.powerDown.play()
-    }
-    if (Checkpoint_counter == 11) {
-        tiles.placeOnRandomTile(person, assets.tile`tile23`)
-        info.changeLifeBy(-1)
-        music.powerDown.play()
-    }
-    if (Checkpoint_counter == 12) {
-        tiles.placeOnRandomTile(person, assets.tile`tile24`)
-        info.changeLifeBy(-1)
-        music.powerDown.play()
-    }
-    if (Checkpoint_counter == 13) {
-        tiles.placeOnRandomTile(person, assets.tile`tile25`)
-        info.changeLifeBy(-1)
-        music.powerDown.play()
-    }
+    doSomething()
 })
 scene.onOverlapTile(SpriteKind.Player, assets.tile`tile25`, function (sprite, location) {
     if (Checkpoint_counter < 13) {
@@ -197,6 +80,9 @@ scene.onOverlapTile(SpriteKind.Player, assets.tile`tile8`, function (sprite, loc
 })
 scene.onOverlapTile(SpriteKind.Player, sprites.dungeon.hazardLava1, function (sprite, location) {
     game.showLongText("You have suffered serious burn wounds. You are transported to the nearest hospital", DialogLayout.Center)
+    doSomething()
+})
+function doSomething () {
     if (Checkpoint_counter == 1) {
         tiles.placeOnRandomTile(person, assets.tile`tile8`)
         info.changeLifeBy(-1)
@@ -262,74 +148,20 @@ scene.onOverlapTile(SpriteKind.Player, sprites.dungeon.hazardLava1, function (sp
         info.changeLifeBy(-1)
         music.powerDown.play()
     }
-})
+    if (Checkpoint_counter == 14) {
+        tiles.placeOnRandomTile(person, assets.tile`tile`)
+        info.changeLifeBy(-1)
+        music.powerDown.play()
+    }
+    if (Checkpoint_counter == 15) {
+        tiles.placeOnRandomTile(person, assets.tile`tile0`)
+        info.changeLifeBy(-1)
+        music.powerDown.play()
+    }
+}
 scene.onOverlapTile(SpriteKind.Player, sprites.dungeon.hazardWater, function (sprite, location) {
     game.showLongText("Your clumsiness has led to your death. You have drowned! You are transported back to the nearest hospital.", DialogLayout.Center)
-    if (Checkpoint_counter == 1) {
-        tiles.placeOnRandomTile(person, assets.tile`tile8`)
-        info.changeLifeBy(-1)
-        music.powerDown.play()
-    }
-    if (Checkpoint_counter == 2) {
-        tiles.placeOnRandomTile(person, assets.tile`tile9`)
-        info.changeLifeBy(-1)
-        music.powerDown.play()
-    }
-    if (Checkpoint_counter == 3) {
-        tiles.placeOnRandomTile(person, assets.tile`tile10`)
-        info.changeLifeBy(-1)
-        music.powerDown.play()
-    }
-    if (Checkpoint_counter == 4) {
-        tiles.placeOnRandomTile(person, assets.tile`tile11`)
-        info.changeLifeBy(-1)
-        music.powerDown.play()
-    }
-    if (Checkpoint_counter == 5) {
-        tiles.placeOnRandomTile(person, assets.tile`tile14`)
-        info.changeLifeBy(-1)
-        music.powerDown.play()
-    }
-    if (Checkpoint_counter == 6) {
-        tiles.placeOnRandomTile(person, assets.tile`tile15`)
-        info.changeLifeBy(-1)
-        music.powerDown.play()
-    }
-    if (Checkpoint_counter == 7) {
-        tiles.placeOnRandomTile(person, assets.tile`tile16`)
-        info.changeLifeBy(-1)
-        music.powerDown.play()
-    }
-    if (Checkpoint_counter == 8) {
-        tiles.placeOnRandomTile(person, assets.tile`tile17`)
-        info.changeLifeBy(-1)
-        music.powerDown.play()
-    }
-    if (Checkpoint_counter == 9) {
-        tiles.placeOnRandomTile(person, assets.tile`tile18`)
-        info.changeLifeBy(-1)
-        music.powerDown.play()
-    }
-    if (Checkpoint_counter == 10) {
-        tiles.placeOnRandomTile(person, assets.tile`tile19`)
-        info.changeLifeBy(-1)
-        music.powerDown.play()
-    }
-    if (Checkpoint_counter == 11) {
-        tiles.placeOnRandomTile(person, assets.tile`tile23`)
-        info.changeLifeBy(-1)
-        music.powerDown.play()
-    }
-    if (Checkpoint_counter == 12) {
-        tiles.placeOnRandomTile(person, assets.tile`tile24`)
-        info.changeLifeBy(-1)
-        music.powerDown.play()
-    }
-    if (Checkpoint_counter == 13) {
-        tiles.placeOnRandomTile(person, assets.tile`tile25`)
-        info.changeLifeBy(-1)
-        music.powerDown.play()
-    }
+    doSomething()
 })
 scene.onOverlapTile(SpriteKind.Player, assets.tile`tile17`, function (sprite, location) {
     if (Checkpoint_counter < 8) {
@@ -356,6 +188,12 @@ scene.onOverlapTile(SpriteKind.Player, assets.tile`tile23`, function (sprite, lo
     if (Checkpoint_counter < 11) {
         Checkpoint_counter += 1
         game.showLongText("Checkpoint 10 reached", DialogLayout.Top)
+    }
+})
+scene.onOverlapTile(SpriteKind.Player, assets.tile`tile0`, function (sprite, location) {
+    if (Checkpoint_counter < 15) {
+        Checkpoint_counter += 1
+        game.showLongText("Last checkpoint reached", DialogLayout.Top)
     }
 })
 scene.onOverlapTile(SpriteKind.Player, assets.tile`tile10`, function (sprite, location) {
@@ -401,3 +239,25 @@ person.ay = 300
 game.showLongText("Press 'A' on screen to continue", DialogLayout.Top)
 game.showLongText("You have been summoned to save the human race from a deadly disease called COVID-19. Against all peril you must find the components to create a vaccine avoiding contagious people and viruses along the way. Each time you respawn at a checkpoint you lose a health", DialogLayout.Top)
 Make_chest_not_happen_again = 0
+let Enemy_1 = sprites.create(img`
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . 7 . . . . . . . . 
+    . . . . . . 7 7 7 . . . . . . . 
+    . . . . 2 f 7 2 7 f 2 . . . . . 
+    . . . . f 2 f 2 f 2 f . . . . . 
+    . . . 7 7 f 2 2 2 f 7 7 . . . . 
+    . . 7 7 2 2 2 2 2 2 2 7 7 . . . 
+    . . . 7 7 f 2 2 2 f 7 7 . . . . 
+    . . . . f 2 f 2 f 2 f . . . . . 
+    . . . . 2 f 7 2 7 f 2 . . . . . 
+    . . . . . . 7 7 7 . . . . . . . 
+    . . . . . . . 7 . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    `, SpriteKind.Enemy)
+tiles.placeOnTile(Enemy_1, tiles.getTileLocation(6, 145))
+forever(function () {
+	
+})
